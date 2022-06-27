2993a59164010bd5f510c412dbecec53c3b612e1
Angelo Matteo <angmatt90@gmail.com>
2022-06-26 21:24:52 -0700

    added UI message for instructions on scenario start

--- levels/gridmap_v2/scenarios/moose_test_angelo234.lua
+++ levels/gridmap_v2/scenarios/moose_test_angelo234.lua
@@ -27,15 +27,15 @@ M.last_sensors_data = {
 -- 7 = zone 3 enter, 8 = zone 3 middle, 9 = zone 3 exit
 local zones_speeds = {}
 
-local acc_data = 
+local acc_data =
 {
   max_forward_acc = 0,
   max_lateral_acc = 0,
   sum_forward_acc = 0,
   sum_lateral_acc = 0,
   num_samples = 0,
-  avg_forward_acc,
-  avg_lateral_acc
+  avg_forward_acc = 0,
+  avg_lateral_acc = 0
 }
 
 local cones_init_pos = {}
@@ -68,31 +68,31 @@ local function addRunToJSONFile(new_run)
     last_run_index = 1
   else
     local added = false
-  
+
     -- Place new entry in correct order by speed
     for k,v in ipairs(runs_data) do
       if new_run.speeds[1] > v.speeds[1] then
         table.insert(runs_data, k, new_run)
-        last_run_index = k     
-        
+        last_run_index = k
+
         added = true
         break
       end
     end
-    
+
     if not added then
       table.insert(runs_data, new_run)
       last_run_index = #runs_data
     end
   end
-  
+
   jsonWriteFile(runs_json_file_dir, runs_data)
 end
 
 local function removeRunFromJSONFile(id)
   table.remove(runs_data, id)
   jsonWriteFile(runs_json_file_dir, runs_data)
-  
+
   if id < last_run_index then
     last_run_index = last_run_index - 1
   elseif id == last_run_index then
@@ -103,7 +103,7 @@ end
 local function removeAllRunsFromJSONFile()
   runs_data = {}
   jsonWriteFile(runs_json_file_dir, runs_data)
-  
+
   last_run_index = -1
 end
 
@@ -125,121 +125,123 @@ local function resetData()
     sum_forward_acc = 0,
     sum_lateral_acc = 0,
     num_samples = 0,
-    avg_forward_acc,
-    avg_lateral_acc
+    avg_forward_acc = 0,
+    avg_lateral_acc = 0
   }
 end
 
 local function failRun(msg)
   if state ~= "failed" then
     local str_msg = msg or ""
-  
-    helper.flashUiMessage("Run Disqualified! " .. str_msg, 5)     
+
+    helper.flashUiMessage("Run Disqualified! " .. str_msg, 5)
     state = "failed"
   end
 end
 
 local function removeOtherObjects()
   local handling_moose_test = scenetree.zone_handling:findObject("handling_moose_test")
-  
+
   for k, v in pairs(handling_moose_test:getObjects()) do
     local obj = handling_moose_test:findObjectById(v)
-    
+
     if obj.shapeName == "/levels/gridmap_v2/art/shapes/grid/s_gm_flip_dome_4x4.dae" then
       obj:setField('position', '', '0 -999 0')
     end
   end
-  
+
   be:reloadCollision()
 end
 
 local function setupCones()
   local veh = be:getPlayerVehicle(0)
-  
+
   -- Section 1
   -- width (m) = 1.1 * vehicle_width + 0.25
   -- left = pos y axis
   local half_width_1 = 1.1 * veh:getSpawnWorldOOBB():getHalfExtents().x + 0.125
-  
+
   local section1_triggers = {"zone_trigger1", "zone_trigger2", "zone_trigger3"}
   local section1_cones = {"cone1l", "cone1r", "cone2l", "cone2r", "cone3l", "cone3r"}
-  
+
   local i = 1
-  
+
   for k, v in pairs(section1_triggers) do
     local trigger = scenetree.findObject(v)
     local trigger_pos = trigger:getPosition()
-    
+
     local cone_l = scenetree.findObject(section1_cones[i])
     local cone_r = scenetree.findObject(section1_cones[i + 1])
-    
+
     cone_l:setPosition(trigger_pos + vec3(0, half_width_1, 0))
     cone_r:setPosition(trigger_pos - vec3(0, half_width_1, 0))
-    
+
     i = i + 2
   end
-  
+
   -- Section 2
   -- width (m) = vehicle_width + 1.0
   local half_width_2 = veh:getSpawnWorldOOBB():getHalfExtents().x + 0.5
-  
+
   local section2_triggers = {"zone_trigger4", "zone_trigger5", "zone_trigger6"}
   local section2_cones = {"cone4l", "cone4r", "cone5l", "cone5r", "cone6l", "cone6r"}
-  
-  local cone3l_pos = scenetree.findObject("cone3l"):getPosition() 
+
+  local cone3l_pos = scenetree.findObject("cone3l"):getPosition()
   local trigger_y = cone3l_pos.y + 1 + half_width_2
 
   i = 1
-  
+
   for k, v in pairs(section2_triggers) do
     local trigger = scenetree.findObject(v)
     local trigger_pos = trigger:getPosition()
-    
+
     trigger:setPosition(vec3(trigger_pos.x, trigger_y, trigger_pos.z))
     trigger_pos = trigger:getPosition()
-    
+
     local cone_l = scenetree.findObject(section2_cones[i])
     local cone_r = scenetree.findObject(section2_cones[i + 1])
-    
+
     cone_l:setPosition(trigger_pos + vec3(0, half_width_2, 0))
     cone_r:setPosition(trigger_pos - vec3(0, half_width_2, 0))
-    
+
     i = i + 2
   end
-  
+
   -- Section 3
   -- width (m) = 3
   local half_width_3 = 1.5
-  
+
   local section3_triggers = {"zone_trigger7", "zone_trigger8", "zone_trigger9"}
   local section3_cones = {"cone7l", "cone7r", "cone8l", "cone8r", "cone9l", "cone9r"}
-  
+
   i = 1
-  
+
   for k, v in pairs(section3_triggers) do
     local trigger = scenetree.findObject(v)
     local trigger_pos = trigger:getPosition()
-    
+
     local cone_l = scenetree.findObject(section3_cones[i])
     local cone_r = scenetree.findObject(section3_cones[i + 1])
-    
+
     cone_l:setPosition(trigger_pos + vec3(0, half_width_3, 0))
     cone_r:setPosition(trigger_pos - vec3(0, half_width_3, 0))
-    
+
     i = i + 2
   end
-  
+
   reset_cone_timer = 0
 end
 
 local function onRaceStart()
   -- Deactivate when watching replay
   if core_replay.state.state == "playing" then return end
-  
+
   removeOtherObjects()
   setupCones()
   resetData()
-  
+
+  helper.flashUiMessage("Start at 70 kmh / 45 mph to get a feel for the vehicle's handling and then increase/decrease speed as needed. No inputs except steering are allowed through the cones!", 10)
+
   next_trigger = 1
   state = "ready"
 end
@@ -247,8 +249,8 @@ end
 local function getUserInputs()
   local playerVehicle = be:getPlayerVehicle(0)
   if not playerVehicle then return end
-    
-  for k, v in pairs(M.last_user_inputs) do    
+
+  for k, v in pairs(M.last_user_inputs) do
     playerVehicle:queueLuaCommand("obj:queueGameEngineLua('if not scenario_moose_test_angelo234 then return end scenario_moose_test_angelo234.last_user_inputs." .. k .. " = ' .. input.state." .. k .. ".val)")
   end
 end
@@ -256,100 +258,99 @@ end
 local function getVehicleSensorsData()
   local playerVehicle = be:getPlayerVehicle(0)
   if not playerVehicle then return end
-    
-  for k, v in pairs(M.last_sensors_data) do    
+
+  for k, v in pairs(M.last_sensors_data) do
     playerVehicle:queueLuaCommand("obj:queueGameEngineLua('if not scenario_moose_test_angelo234 then return end scenario_moose_test_angelo234.last_sensors_data." .. k .. " = ' .. sensors." .. k .. ")")
   end
-  
+
   acc_data.max_forward_acc = math.max(M.last_sensors_data.gy2, acc_data.max_forward_acc)
   acc_data.max_lateral_acc = math.max(math.abs(M.last_sensors_data.gx2), acc_data.max_lateral_acc)
-  
-  -- 
+
+  --
   acc_data.sum_forward_acc = acc_data.sum_forward_acc + M.last_sensors_data.gy2
   acc_data.sum_lateral_acc = acc_data.sum_lateral_acc + math.abs(M.last_sensors_data.gx2)
   acc_data.num_samples = acc_data.num_samples + 1
-  
+
   acc_data.avg_forward_acc = acc_data.sum_forward_acc / acc_data.num_samples
   acc_data.avg_lateral_acc = acc_data.sum_lateral_acc / acc_data.num_samples
 end
 
 local function failPlayerOnInputs()
-  for k, v in pairs(M.last_user_inputs) do    
+  for k, v in pairs(M.last_user_inputs) do
     if k ~= "steering" and v > 0 and state == "running" then
       failRun("User input except steering not allowed!")
     end
-  end 
+  end
 end
 
 local function renderUIText(dt)
   if update_ui_timer >= update_ui_delay then
     local speed = 0
-    
+
     guihooks.message("Last Exit Speed: " .. string.format("%.1f", round(speed * 3.6, 1)) .. " km/h", 2 * update_ui_delay, "num_deliveries")
-  
+
     update_ui_timer = 0
   end
-  
+
   update_ui_timer = update_ui_timer + dt
 end
 
+local im_char_size = 6.25
+local x_button_size = 15
+
 local function renderIMGUI()
   if not show_window then return end
-  
-  local im_char_size = 6.25
-  local x_button_size = 15
-  
+
   if im.Begin('Leaderboards', window_open, im.WindowFlags_MenuBar) then
     if im.BeginMenuBar() then
       if im.BeginMenu("File") then
         --im.PushStyleColor2(im.Col_Text, im.ImVec4(1, 0, 0, 1))
         --im.PopStyleColor()
-      
+
         if im.MenuItem1("Remove All Runs") then
           removeAllRunsFromJSONFile()
         end
         im.EndMenu()
       end
-      
-      
+
       im.EndMenuBar()
-    end 
-    
+    end
+
     for k,v in ipairs(runs_data) do
       if v.speeds and v.acc_data and v.config_name then
         local str_entrance_speed = string.format("%.1f", round(v.speeds[1] * 3.6, 1))
         local str_middle_speed = string.format("%.1f", round(v.speeds[5] * 3.6, 1))
         local str_exit_speed = string.format("%.1f", round(v.speeds[9] * 3.6, 1))
-        
+
         local str_max_fwd_acc = string.format("%.2f", round(-v.acc_data.max_forward_acc / 9.80665, 2))
         local str_max_lat_acc = string.format("%.2f", round(v.acc_data.max_lateral_acc / 9.80665, 2))
         local str_avg_fwd_acc = string.format("%.2f", round(-v.acc_data.avg_forward_acc / 9.80665, 2))
         local str_avg_lat_acc = string.format("%.2f", round(v.acc_data.avg_lateral_acc / 9.80665, 2))
-        
+
         local avail = im.GetContentRegionAvail().x - x_button_size
-        
+
         local full_text = tostring(k) .. ". " .. str_entrance_speed .. " km/h, " .. v.config_name
         local text_displayed = string.sub(full_text, 1, math.floor(avail / im_char_size))
-        
-        if last_run_index == k then      
+
+        if last_run_index == k then
           im.PushFont3("cairo_bold")
           im.Text(text_displayed)
           im.PopFont()
         else
           im.Text(text_displayed)
         end
-        
-        local tooltip_text = 
+
+        local tooltip_text =
           v.config_name .. "\n"
           .. "Entrance/Middle/Exit Speed: " .. str_entrance_speed .. "/" .. str_middle_speed .. "/" .. str_exit_speed .. " km/h\n"
           .. "Max Forward/Lateral Acceleration: " .. str_max_fwd_acc .. "/" .. str_max_lat_acc .. " g's\n"
           .. "Avg Forward/Lateral Acceleration: " .. str_avg_fwd_acc .. "/" .. str_avg_lat_acc .. " g's"
-          
-        
+
+
         im.tooltip(tooltip_text)
-        
+
         im.SameLine(avail)
-        
+
         --im.PushStyleColor2(im.Col_Text, im.ImVec4(1, 0, 0, 1))
         if im.Button("X##" .. tostring(k)) then
           removeRunFromJSONFile(k)
@@ -367,13 +368,13 @@ local function resetConeTimer(dt)
     for k,v in pairs(map.objects) do
       cones_init_pos[k] = vec3(v.pos)
     end
-    
+
     -- Track cones for movement
     be:queueAllObjectLua("mapmgr.enableTracking()")
-    
+
     reset_cone_timer = -1
   end
-  
+
   if reset_cone_timer >= 0 then
     reset_cone_timer = reset_cone_timer + dt
   end
@@ -381,18 +382,18 @@ end
 
 local function onUpdate(dt)
   -- Display UI text
-  --renderUIText(dt) 
+  --renderUIText(dt)
   renderIMGUI()
-  
+
   -- Deactivate when watching replay
   if core_replay.state.state == "playing" then return end
-  
+
   resetConeTimer(dt)
-  
+
   if state == "running" then
-    getUserInputs() 
-    getVehicleSensorsData() 
-    
+    getUserInputs()
+    getVehicleSensorsData()
+
     failPlayerOnInputs()
   end
 end
@@ -400,15 +401,15 @@ end
 local function onRaceTick(raceTickTime, scenarioTimer)
   -- Deactivate when watching replay
   if core_replay.state.state == "playing" then return end
-  
+
   local playerVeh = be:getPlayerVehicle(0)
-  
-  if tableSize(cones_init_pos) > 0 and reset_cone_timer == -1 then 
+
+  if tableSize(cones_init_pos) > 0 and reset_cone_timer == -1 then
     for k,v in pairs(map.objects) do
       if v.id ~= playerVeh:getID() then
         local dist_moved = (v.pos - cones_init_pos[k]):length()
-        
-        if dist_moved > 0.1 then  
+
+        if dist_moved > 0.1 then
           -- Cone hit!!!
           failRun("You hit a cone!")
           break
@@ -420,9 +421,9 @@ end
 
 local function resetScene()
   local playerVeh = be:getPlayerVehicle(0)
-  
+
   reset_cone_timer = 0
-  
+
   -- Move all cones back to original positions
   local allVehicles = scenetree.findClassObjects('BeamNGVehicle')
   for k, vehicleName in ipairs(allVehicles) do
@@ -432,25 +433,25 @@ local function resetScene()
       vehicle:reset()
     end
   end
-  
+
   -- Set vehicle position back to original position
   -- but with current velocity
   playerVeh:setPositionNoPhysicsReset(spawn_pos)
-  
+
   resetData()
-  
+
   next_trigger = 1
   state = "ready"
 end
 
 local function setSpeedAndVerify(i)
   local speed = be:getPlayerVehicle(0):getVelocity():length()
-  
+
   if next_trigger == i then
     zones_speeds[i] = speed
-  
+
     next_trigger = i + 1
-  
+
   elseif zones_speeds[i] == nil then
     failRun()
   end
@@ -460,66 +461,66 @@ local function successfulRun()
   local config_data = core_vehicles.getCurrentVehicleDetails().configs
   local veh_config = config_data.Name
   local veh_name = nil
-  
+
   if config_data.aggregates.Brand then
     local veh_brand = tableKeys(config_data.aggregates.Brand)[1]
     veh_name = veh_brand .. " " .. veh_config
   else
     veh_name = veh_config
   end
-  
+
   -- Save speed to file
   addRunToJSONFile({
       config_name = veh_name,
       speeds = zones_speeds,
       acc_data = acc_data
   })
-  
+
   helper.flashUiMessage("Successful Run!", 3)
-  
+
   state = "finished"
 end
 
 local function onBeamNGTrigger(data)
   -- Deactivate when watching replay
   if core_replay.state.state == "playing" then return end
-  
+
   local veh = be:getPlayerVehicle(0)
-  
+
   -- Only care about player on triggers
   if not veh or data.subjectName ~= "thePlayer" then return end
-  
+
   if data.triggerName == "zone_trigger1" then
     if data.event == "enter" then
       state = "running"
-    
+
       setSpeedAndVerify(1)
     end
-  
+
   elseif data.triggerName == "zone_trigger9" then
     if data.event == "enter" then
       setSpeedAndVerify(9)
-      
+
     elseif data.event == "exit" then
       if state == "running" then
         successfulRun()
       end
     end
-    
+
   elseif data.triggerName == "finish_trigger" then
     if data.event == "enter" then
       resetScene()
     end
-  
+
   elseif data.triggerName:find("zone_trigger") then
     if data.event == "enter" then
       local str_id = data.triggerName:match("%d+")
       local id = tonumber(str_id)
-      
+
       setSpeedAndVerify(id)
     end
   end
-  
+
 end
 
 M.onScenarioUIReady = onScenarioUIReady
